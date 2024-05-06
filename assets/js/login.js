4a39dcb319a0c81c2d518c493438819ba587a1af
phuc1801 <nguyenphuc200x@gmail.com>
2024-05-06 16:38:24 +0700

    build-login show, hide btn

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,27 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const showpBtn = document.querySelector('.sign-up-btn');
+const hideBtn1 = document.querySelector('.close-btnX1');
+const hideBtn2 = document.querySelector('.close-btnX2');
 
+
+// mo sign in
+
+showpBtn.addEventListener("click", () =>{
+    document.body.classList.toggle("show-popup");
+});
+
+hideBtn1.addEventListener("click", () => {
+    document.body.classList.remove("show-popup");
+});
+
+hideBtn2.addEventListener("click", () => {
+    document.body.classList.remove("show-popup");
+});
+
+
+// chuyen sign up voi sign in
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
