b70b352f32365f445b1ebc3d6a751fbae4988618
phuc1801 <nguyenphuc200x@gmail.com>
2024-06-03 08:21:02 +0700

    update forgot password

--- connect.php
+++ connect.php
@@ -1,8 +1,3 @@
 <?php
 $conn = new PDO('mysql:host=localhost;dbname=qlbh', 'root' ,'');
-if ($conn) {
-    echo "Connected successfully";
-}else{
-    echo "Connection failed";
-}
 ?>
--- forgot_password.php
+++ forgot_password.php
@@ -68,31 +68,73 @@
     }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../css/forgot.css?v=<?php echo time(); ?>">
-    <title>Forgot</title>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>Document</title>
 </head>
 <body>
-   <form action="" method="post" style="width: 600px;" class="border border-primary border-2 m-auto p-2">
-        <h4 class="mb-3 text-center">Quên mật khẩu</h4>
-        <?php if ($loi != ""): ?>
-        <div class="alert <?php echo ($count == 0) ? 'alert-danger' : 'alert-success'; ?>">
-            <?php echo $loi; ?>
+    <!-- Password Reset 8 - Bootstrap Brain Component -->
+<section class="bg-light p-3 p-md-4 p-xl-5">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-xxl-11">
+        <div class="card border-light-subtle shadow-sm">
+          <div class="row g-0">
+            <div class="col-12 col-md-6">
+              <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src="./assets/img/the-coffee-house.jpeg" alt="Welcome back you've been missed!">
+            </div>
+            <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
+              <div class="col-12 col-lg-11 col-xl-10">
+                <div class="card-body p-3 p-md-4 p-xl-5">
+                  <div class="row">
+                    <div class="col-12">
+                      <div class="mb-5">
+                        <div class="text-center mb-4">
+                          <a href="#!">
+                            <img src="./assets/img/logo.svg" alt="BootstrapBrain Logo" width="175" height="57">
+                          </a>
+                        </div>
+                        <h2 class="h4 text-center">Password Reset</h2>
+                        <h3 class="fs-6 fw-normal text-secondary text-center m-0">Provide the email address associated with your account to recover your password.</h3>
+                      </div>
+                    </div>
+                  </div>
+                  <form action="#!">
+                    <div class="row gy-3 overflow-hidden">
+                      <div class="col-12">
+                        <div class="form-floating mb-3">
+                          <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
+                          <label for="email" class="form-label">Email</label>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="d-grid">
+                          <button class="btn btn-dark btn-lg" type="submit">Reset Password</button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                  <div class="row">
+                    <div class="col-12">
+                      <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-5">
+                        <a href="login.php" class="link-secondary text-decoration-none">Login</a>
+                        <a href="login.php" class="link-secondary text-decoration-none">Register</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <?php endif; ?>
-        <div class="mb-3 ip-form">
-            <label for="email" class="form-label">Nhập Email</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-            <button type="submit" class="btn btn-primary mt-3" name="send">Gửi yêu cầu</button>
-            <a href="login.php" class="btn btn-secondary mt-3">Quay lại</a>
-        </div>
-   </form>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
